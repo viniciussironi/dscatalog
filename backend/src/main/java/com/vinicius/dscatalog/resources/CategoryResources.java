7a11a10972f80e77cc1a541ca5015b1de62aaf9d
Vinicius Sironi <viniciussironi@gmail.com>
2024-01-16 15:01:00 -0300

    H2 database

--- backend/src/main/java/com/vinicius/dscatalog/resources/CategoryResources.java
+++ backend/src/main/java/com/vinicius/dscatalog/resources/CategoryResources.java
@@ -3,21 +3,24 @@ package com.vinicius.dscatalog.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.vinicius.dscatalog.entities.Category;
+import com.vinicius.dscatalog.services.CategoryService;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResources {
 	
+	@Autowired
+	private CategoryService service;
+	
 	@GetMapping
 	public ResponseEntity<List<Category>> findAll() {
-		List<Category> list = new ArrayList<>();
-		list.add(new Category(1L, "anfsafn"));
-		return ResponseEntity.ok().body(list);
+		return ResponseEntity.ok().body(service.findAll());
 	}
 }
---
+++ backend/src/main/java/com/vinicius/dscatalog/services/CategoryService.java
@@ -0,0 +1,20 @@
+package com.vinicius.dscatalog.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.vinicius.dscatalog.entities.Category;
+import com.vinicius.dscatalog.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll() { 
+		return repository.findAll();
+	}
+}
